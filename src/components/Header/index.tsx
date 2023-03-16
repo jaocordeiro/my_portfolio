a7adfcdab1a191f3f9eed8bf8161256193aa9cc4
@joaocordeiro <joaocordeiroalmeida01@gmail.com>
2023-03-16 16:03:46 -0300

    refactor: ajusted Header for mobile and web

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,21 +5,22 @@ import GitHub from '@/components/Svgs/GitHub';
 
 export const Header = () => {
   return (
-    <header className="w-full h-20 bg-mpBlackHeader px-5 flex">
+    <header className="w-full h-20 xl:h-24 bg-mpBlackHeader px-7 md:px-16 xl:px-36 flex">
       <div className="w-full flex items-center ">
-        <div className="">
-          <CodeIcon width={40} height={40} fill="#ffffff" />
+        <div className="flex items-center">
+          <CodeIcon className="w-10 h-10 xl:w-14 xl:h-14" fill="#ffffff" />
+          <p className="text-white pl-4 text-xl font-bold max-md:hidden">CODE</p>
         </div>
       </div>
       <div className="flex items-center">
-        <div className="pr-4">
+        <div className="pr-4 xl:pr-7">
           <Link href="https://www.linkedin.com/in/jo%C3%A3o-vitor-cordeiro-de-almeida-6033b9147/" target="_blank">
-            <LinkedinIcon className="w-7 h-7" />
+            <LinkedinIcon className="w-7 h-7 xl:w-10 xl:h-10" />
           </Link>
         </div>
         <div>
           <Link href="https://github.com/jaocordeiro" target="_blank">
-            <GitHub className="w-7 h-7" />
+            <GitHub className="w-7 h-7 xl:w-10 xl:h-10" />
           </Link>
         </div>
       </div>
