c557c5e0a99cde42de261ef0a9aae28fef35a409
@joaocordeiro <joaocordeiroalmeida01@gmail.com>
2023-03-22 17:57:34 -0300

    add: assets for carousel

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,10 +10,18 @@ module.exports = {
     extend: {
       colors: {
         mpBlackHeader: '#212121',
+        mpLightBlack: '#1A1A1A',
         mpBlack: '#161616',
         mpLightGray: '#DFDFDF',
         mpGrey: '#a5a5a5',
         mpDarkGrey: '#696969',
+      },
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        md: '2rem',
       },
     },
   },
