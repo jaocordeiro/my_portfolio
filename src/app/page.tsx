f49afa5ce561983187b1ba6b713a2cb2c73abd6a
@joaocordeiro <joaocordeiroalmeida01@gmail.com>
2023-03-28 22:34:29 -0300

    refactor: Component Accordion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,33 @@ import { Header } from '@/components/Header';
 import { Write } from '@/components/TypeWriter';
 import { Carousel } from '@/components/Carousel';
 import { Accordion } from '@/components/Accordion';
+import { useEffect, useState } from 'react';
+
+type SetState = {
+  id: string;
+  html_url: string;
+  name: string;
+  language: string;
+};
 
 export default function Home() {
+  const [repos, setRepos] = useState<SetState[]>([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    fetch('https://api.github.com/users/jaocordeiro/repos')
+      .then((res: { json: () => any }) => res.json())
+      .then((repos) => {
+        setRepos(repos);
+        setLoading(false);
+      });
+  }, []);
+
+  const specifyRepos: SetState[] = repos.slice(0, 6);
+
+  const outhersRepos: SetState[] = repos.slice(7);
+
   return (
     <div>
       <div className="bg-mpBlack h-screen">
@@ -15,7 +40,7 @@ export default function Home() {
       </div>
       <Carousel />
 
-      <Accordion />
+      <Accordion specifyRepos={specifyRepos} outhersRepos={outhersRepos} />
     </div>
   );
 }
--- src/components/Accordion/index.tsx
+++ src/components/Accordion/index.tsx
@@ -1,53 +1,61 @@
 'use client';
-import { useEffect, useState } from 'react';
 import Link from 'next/link';
+import { useState } from 'react';
+import { Collapse } from 'react-collapse';
+import UpArrowIcon from '@/components/Svgs/UpArrow';
+import DownArrowIcon from '@/components/Svgs/DownArrow';
 
-type SetState = {
-  id: string;
-  html_url: string;
-  name: string;
-  language: string;
-};
-
-export function Accordion() {
-  const [repos, setRepos] = useState<SetState[]>([]);
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    setLoading(true);
-    fetch('https://api.github.com/users/jaocordeiro/repos')
-      .then((res: { json: () => any }) => res.json())
-      .then((repos) => {
-        setRepos(repos);
-        setLoading(false);
-      });
-  }, []);
+export function Accordion({ specifyRepos, outhersRepos }: any) {
+  const [open, setOpen] = useState<boolean>(false);
 
   return (
     <section className="w-full h-full bg-mpBlack pt-16 pb-7">
       <p className="text-mpGrey text-center mb-14 text-2xl font-bold">Repositório</p>
-      <div className="container mx-auto grid sm:grid-cols-1 text-center justify-items-center md:grid-cols-3 gap-y-2.5 gap-x-2.5 px-0">
-        {repos.map((item) => {
-          return (
-            <div
-              key={item.id}
-              className="flex md:flex justify-center items-center w-4/5 h-24 bg-mpLightBlack border-solid border-2 border-mpBlackHeader"
-            >
-              <div className=""></div>
+      <div className="container mx-auto grid sm:grid-cols-1 text-center justify-items-center md:grid-cols-3 gap-y-4 gap-x-2.5 px-0">
+        {specifyRepos.map(
+          (repos: { id: number; html_url: string; name: string; language: string }) => {
+            return (
               <Link
-                href={item.html_url}
+                key={repos.id}
+                href={repos.html_url}
                 target="_blank"
-                className="text-mpDarkGrey font-bold text-lg"
+                className="text-mpDarkGrey py-1 px-1 font-bold text-lg grid justify-center items-center w-4/5 h-24 bg-mpLightBlack border-solid border-2 border-mpBlackHeader"
               >
-                {item.name}
+                <p>{repos.name}</p>
                 <p className="text-mpGrey text-sm">
-                  {item.language ? item.language : 'TypeScript'}
+                  {repos.language ? repos.language : 'TypeScript'}
                 </p>
               </Link>
-            </div>
-          );
-        })}
+            );
+          },
+        )}
       </div>
+      <div onClick={() => setOpen(!open)} className="h-7 w-full flex justify-center pt-6">
+        <button className="text-white cursor-pointer">
+          {open ? <UpArrowIcon className="w-6" /> : <DownArrowIcon className="w-6" />}
+        </button>
+      </div>
+      <Collapse isOpened={open} open={open}>
+        <div className="grid md:grid-cols-3 text-center justify-items-center pt-11 gap-y-4 gap-x-2.5">
+          {outhersRepos.map(
+            (item: { id: number; html_url: string; name: string; language: string }) => {
+              return (
+                <Link
+                  key={item.id}
+                  href={item.html_url}
+                  target="_blank"
+                  className="text-mpDarkGrey py-1 px-1 font-bold text-lg grid justify-center items-center w-4/5 h-24 bg-mpLightBlack border-solid border-2 border-mpBlackHeader"
+                >
+                  <p>{item.name}</p>
+                  <p className="text-mpGrey text-sm">
+                    {item.language ? item.language : 'TypeScript'}
+                  </p>
+                </Link>
+              );
+            },
+          )}
+        </div>
+      </Collapse>
     </section>
   );
 }
