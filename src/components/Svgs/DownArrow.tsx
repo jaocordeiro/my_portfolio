66e86c655a4abfc48f98773a99d72be1c6e6ffac
@joaocordeiro <joaocordeiroalmeida01@gmail.com>
2023-03-28 22:35:30 -0300

    feat: create Svgs Arrow for Component Accordion

---
+++ src/components/Svgs/DownArrow.tsx
@@ -0,0 +1,12 @@
+import { SVGProps } from 'react';
+
+function DownArrow({ className }: SVGProps<SVGSVGElement>) {
+  return (
+    <svg className={className} fill="#9e9e9e" viewBox="0 0 24 24">
+      <path d="M12 17.586L4.707 10.293 3.293 11.707 12 20.414 20.707 11.707 19.293 10.293 12 17.586z" />
+      <path d="M20.707 5.707L19.293 4.293 12 11.586 4.707 4.293 3.293 5.707 12 14.414 20.707 5.707z" />
+    </svg>
+  );
+}
+
+export default DownArrow;
---
+++ src/components/Svgs/UpArrow.tsx
@@ -0,0 +1,12 @@
+import { SVGProps } from 'react';
+
+function UpArrow({ className }: SVGProps<SVGSVGElement>) {
+  return (
+    <svg className={className} fill="#9e9e9e" viewBox="0 0 24 24">
+      <path d="M12 6.414L19.293 13.707 20.707 12.293 12 3.586 3.293 12.293 4.707 13.707 12 6.414z" />
+      <path d="M3.293 18.293L4.707 19.707 12 12.414 19.293 19.707 20.707 18.293 12 9.586 3.293 18.293z" />
+    </svg>
+  );
+}
+
+export default UpArrow;
